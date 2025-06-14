acab397524de2a9e3c9839c9950d7e7908a53776
Vladislav Ulyanov <flynnrcore@yandex.ru>
2025-06-14 10:30:54 +0300

    Фикс ссылки возврата на главную страницу

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 type TErrorPage = { picUrl: string; message: string };
 
 const ErrorPage = ({ picUrl, message }: TErrorPage) => {
@@ -5,9 +7,7 @@ const ErrorPage = ({ picUrl, message }: TErrorPage) => {
     <div className="flex h-screen flex-col items-center justify-center bg-stone-50">
       <img src={picUrl} alt="Error img" className="mb-6 h-1/2" />
       <h1 className="text-4xl font-bold">{message}</h1>
-      <a href="/" className="mt-6 text-yellow-500 hover:underline">
-        Вернуться на главную
-      </a>
+      <Link to="/" className="mt-6 text-yellow-500 hover:underline">Вернуться на главную</Link>
     </div>
   );
 };
