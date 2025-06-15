795156584f759eb2f9210a80e61a104b23000fe8
Vladislav Ulyanov <flynnrcore@yandex.ru>
2025-06-15 22:58:42 +0300

    Удалены неиспользуемые импорты

--- src/components/DashboardPageContent/DifficultyStatistic.tsx
+++ src/components/DashboardPageContent/DifficultyStatistic.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as React from 'react';
-import { Area, AreaChart, CartesianGrid, Line, LineChart, XAxis } from 'recharts';
+import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { type ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
