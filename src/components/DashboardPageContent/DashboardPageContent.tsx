fc2b30b69c1c0b44af076831e6acf79e545c15dd
Vladislav Ulyanov <flynnrcore@yandex.ru>
2025-06-17 14:03:35 +0300

    изменение позицирования контента

--- src/components/DashboardPageContent/DashboardPageContent.tsx
+++ src/components/DashboardPageContent/DashboardPageContent.tsx
@@ -6,7 +6,7 @@ import { DifficultyStatistic } from './DifficultyStatistic';
 const DashboardPageContent = () => {
   const { stats, trainings } = useTrainings();
   return (
-    <div className="flex min-h-screen flex-col bg-stone-50 px-4 py-6 pt-25 sm:px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-32">
+    <div className="flex min-h-screen gap-4 flex-col bg-stone-50 px-4 py-6 pt-25 sm:gap-4 sm:px-6 md:px-8 lg:px-12 xl:px-20 2xl:px-32">
       <div className="flex w-full max-w-7xl items-start">
         <h2 className="text-2xl font-bold sm:text-3xl">Дашборд</h2>
       </div>
