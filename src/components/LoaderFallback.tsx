6b774f1f8feeb451559d46eac960c966f3850d78
Vladislav Ulyanov <flynnrcore@yandex.ru>
2025-06-19 12:12:41 +0300

    добавлены адаптивные стили

--- src/components/LoaderFallback.tsx
+++ src/components/LoaderFallback.tsx
@@ -1,8 +1,12 @@
 import { withBaseUrl } from '@/constants/paths';
 
 const LoaderFallback = () => (
-  <div className="flex h-svh items-center justify-center">
-    <img className="h-1/3" src={withBaseUrl('loader.webp')} alt="Загрузка..." />
+  <div className="flex h-svh items-center justify-center px-4">
+    <img
+      className="h-1/3 max-h-60 w-auto max-w-full sm:max-h-72 md:max-h-96 lg:max-h-[50rem] xl:max-h-[60rem]"
+      src={withBaseUrl('loader.webp')}
+      alt="Загрузка..."
+    />
   </div>
 );
 
