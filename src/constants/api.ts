d190d081cc00f3099744a6bcc07534d2674f8cc6
Vladislav Ulyanov <flynnrcore@yandex.ru>
2025-06-19 17:36:26 +0300

    настройка проксирования

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,10 +1,10 @@
 export const API_URLS = {
-  login: 'https://ninjatracker-backend.onrender.com/api/login',
-  register: 'https://ninjatracker-backend.onrender.com/api/register',
-  logout: 'https://ninjatracker-backend.onrender.com/api/logout',
-  csrf: 'https://ninjatracker-backend.onrender.com/api/csrf-token',
-  session: 'https://ninjatracker-backend.onrender.com/api/session',
-  refresh: 'https://ninjatracker-backend.onrender.com/api/refresh',
-  trainings: 'https://ninjatracker-backend.onrender.com/api/trainings',
-  statistics: 'https://ninjatracker-backend.onrender.com/api/trainings/stats',
+  login: '/api/login',
+  register: '/api/register',
+  logout: '/api/logout',
+  csrf: '/api/csrf-token',
+  session: '/api/session',
+  refresh: '/api/refresh',
+  trainings: '/api/trainings',
+  statistics: '/api/trainings/stats',
 };
