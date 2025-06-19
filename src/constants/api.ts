5c75360bf4d4558d944224143438acd236989cd8
Vladislav Ulyanov <flynnrcore@yandex.ru>
2025-06-19 15:55:54 +0300

    Фикс путей api

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,10 +1,10 @@
 export const API_URLS = {
-  login: 'http://ninjatracker-backend.onrender.com/api/login',
-  register: 'http://ninjatracker-backend.onrender.com/api/register',
-  logout: 'http://ninjatracker-backend.onrender.com/api/logout',
-  csrf: 'http://ninjatracker-backend.onrender.com/api/csrf-token',
-  session: 'http://ninjatracker-backend.onrender.com/api/session',
-  refresh: 'http://ninjatracker-backend.onrender.com/api/refresh',
-  trainings: 'http://ninjatracker-backend.onrender.com/api/trainings',
-  statistics: 'http://ninjatracker-backend.onrender.com/api/trainings/stats',
+  login: 'https://ninjatracker-backend.onrender.com/api/login',
+  register: 'https://ninjatracker-backend.onrender.com/api/register',
+  logout: 'https://ninjatracker-backend.onrender.com/api/logout',
+  csrf: 'https://ninjatracker-backend.onrender.com/api/csrf-token',
+  session: 'https://ninjatracker-backend.onrender.com/api/session',
+  refresh: 'https://ninjatracker-backend.onrender.com/api/refresh',
+  trainings: 'https://ninjatracker-backend.onrender.com/api/trainings',
+  statistics: 'https://ninjatracker-backend.onrender.com/api/trainings/stats',
 };
