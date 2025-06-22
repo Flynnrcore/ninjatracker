99a17d083ff6db5416d2ef2a3c0813c65a1ff347
Vladislav Ulyanov <flynnrcore@yandex.ru>
2025-06-22 12:06:54 +0300

    Фикс проверки разрешения экрана

--- src/hooks/useViewMode.ts
+++ src/hooks/useViewMode.ts
@@ -1,23 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { DESKTOP_BREAKPOINT } from '@/constants/consts';
 
 export const useViewMode = () => {
   const [viewMode, setViewMode] = useState<'table' | 'cards'>(() =>
     window.innerWidth >= DESKTOP_BREAKPOINT ? 'table' : 'cards',
   );
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= DESKTOP_BREAKPOINT) {
-        setViewMode('table');
-      } else {
-        setViewMode('cards');
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
 
   return {
     viewMode,
