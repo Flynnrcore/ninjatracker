8520556352f5b3305ee07105b046fd7061419d9c
Vladislav Ulyanov <flynnrcore@yandex.ru>
2025-07-03 14:21:34 +0300

    Добавлены тесты главной страницы и страницы тренировок

--- tests/pages/NewTrainPage.test.tsx
+++ tests/pages/NewTrainPage.test.tsx
@@ -1,9 +1,9 @@
 import React, { Suspense } from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import DashboardPage from '../../src/pages/DashboardPage';
 import { AuthContext } from '../../src/context/AuthContext';
-import { MemoryRouter } from 'react-router-dom';
 import { mockContext } from '../testUtils/mockAuthContext';
+import NewTrainPage from '../../src/pages/NewTrainPage';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen, waitFor } from '@testing-library/react';
 import { vi } from 'vitest';
 
 beforeAll(() => {
@@ -89,23 +89,23 @@ afterAll(() => {
   vi.unstubAllGlobals();
 });
 
-describe('Тест страницы DashboardPage', () => {
+describe('Тест страницы NewTrainPage', () => {
   const renderWithProviders = () =>
     render(
       <MemoryRouter>
         <AuthContext.Provider value={{ ...mockContext }}>
           <Suspense fallback={<div>Загрузка</div>}>
-            <DashboardPage />
+            <NewTrainPage />
           </Suspense>
         </AuthContext.Provider>
       </MemoryRouter>,
     );
 
-  it('отображает "Общее количество тренировок" после загрузки', async () => {
+  it('отображает форму тренировки', async () => {
     renderWithProviders();
     await waitFor(
       () => {
-        expect(screen.getByText(/Общее количество тренировок/i)).toBeTruthy();
+        expect(screen.findByText(/Таймер тренировки/i)).toBeTruthy();
       },
       { timeout: 10000, interval: 100 }
     );
