61bba384f216b571163e31351cf470733669c7b0
Vladislav Ulyanov <flynnrcore@yandex.ru>
2025-07-08 11:06:49 +0300

    Убрано отступ в стилях секции на главной странице

--- src/components/MainPageContent/MainPageContent.tsx
+++ src/components/MainPageContent/MainPageContent.tsx
@@ -51,7 +51,7 @@ const MainPageContent = () => {
 
       <Section title="" className="max-w-7xl">
         <div className="flex w-full flex-col gap-8 lg:flex-row">
-          <div className="order-2 flex w-full flex-col gap-8 rounded-2xl bg-white p-6 shadow-lg sm:order-1 md:p-8 lg:w-1/2">
+          <div className="order-2 flex w-full flex-col rounded-2xl bg-white p-6 shadow-lg sm:order-1 md:p-8 lg:w-1/2">
             <h2>Планирование тренировок и репертуара</h2>
             {MAIN_PAGE.shedule.children.map((item, index) => (
               <ListFeature key={index} title={item.title} description={item.description} />
